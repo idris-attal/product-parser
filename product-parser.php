64d861c7767198981b3370bffd305ec8372d29b9
idris-attal <i.attal.npa@gmail.com>
2023-12-24 19:00:40 +0100

    product-parser for csv file format

---
+++ product-parser.php
@@ -0,0 +1,155 @@
+<?php
+
+// represent product from the CSV file
+class Product
+{
+    public $make;
+    public $model;
+    public $colour;
+    public $capacity;
+    public $network;
+    public $grade;
+    public $condition;
+
+    public function __construct($make = null, $model = null, $colour = null, $capacity = null, $network = null, $grade = null, $condition = null)
+    {
+        $this->make = $make;
+        $this->model = $model;
+        $this->colour = $colour;
+        $this->capacity = $capacity;
+        $this->network = $network;
+        $this->grade = $grade;
+        $this->condition = $condition;
+    }
+}
+
+// parser class
+class Parser
+{
+    public function parseFile($filename)
+    {
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+        switch (strtolower($extension)) {
+            case 'csv':
+                $this->parseCSV($filename);
+                break;
+            case 'json':
+                $this->parseJSON($filename);
+                break;
+            case 'xml':
+                $this->parseXML($filename);
+                break;
+            default:
+                throw new Exception("Unsupported file format: $extension");
+        }
+    }
+
+    // CSV parsing logic here
+    public function parseCSV($filename)
+    {
+        $file = fopen($filename, "r");
+
+        if ($file !== false) {
+            // Reading header to get the column names
+            $headers = fgetcsv($file);
+
+            while (($row = fgetcsv($file)) !== false) {
+                // Continue rest of the processing
+                $product = $this->createProduct($headers, $row);
+                $this->displayProduct($product);
+                $this->countUniqueCombinations($product);
+            }
+            fclose($file);
+
+            // Write unique combinations to a file
+            $this->writeUniqueCombinationsToFile("combination_count.csv");
+        } else {
+            echo "Error opening file: $filename";
+        }
+    }
+
+    private function createProduct($headers, $row)
+    {
+        // required fields
+        $requiredFields = ['brand_name', 'model_name'];
+
+        foreach ($requiredFields as $requiredField) {
+            $index = array_search($requiredField, $headers);
+            if ($index === false || !isset($row[$index])) {
+                throw new Exception("Required field '$requiredField' not found in the row.");
+            }
+        }
+
+        // Map headers to desired headers
+        $headerMapping = [
+            'brand_name' => 'make',
+            'model_name' => 'model',
+            'colour_name' => 'colour',
+            'gb_spec_name' => 'capacity',
+            'network_name' => 'network',
+            'grade_name' => 'grade',
+            'condition_name' => 'condition',
+        ];
+
+        $product = new Product();
+
+        foreach ($headers as $index => $header) {
+            $value = $row[$index] ?? ''; // Use default value if the column is missing
+            $product->{$headerMapping[$header]} = $value;
+        }
+
+        return $product;
+    }
+
+    private function displayProduct($product)
+    {
+        // Display product information
+        var_dump($product);
+    }
+
+    private $uniqueCombinations = [];
+    private function countUniqueCombinations($product)
+    {
+        // Count unique combinations
+        $combinationKey = json_encode($product);
+        $this->uniqueCombinations[$combinationKey] = ($this->uniqueCombinations[$combinationKey] ?? 0) + 1;
+    }
+
+    private function writeUniqueCombinationsToFile($outputFilename)
+    {
+        // Write unique combinations to the file
+        $file = fopen($outputFilename, "w");
+
+        if ($file !== false) {
+            // Write header
+            $header = array_merge(array_keys(json_decode(key($this->uniqueCombinations), true)), ['count']);
+            fputcsv($file, $header);
+
+            foreach ($this->uniqueCombinations as $combination => $count) {
+                $data = json_decode($combination, true);
+                $data['count'] = $count;
+                fputcsv($file, $data);
+            }
+
+            fclose($file);
+        } else {
+            echo "Error opening file for writing: $outputFilename";
+        }
+    }
+
+    // TODO for future development...
+    private function parseJSON($filename)
+    {
+        // TODO...JSON parsing logic here
+    }
+
+    private function parseXML($filename)
+    {
+        // TODO.. XML parsing logic here
+    }
+}
+
+// calling for usage
+$parser = new Parser();
+$parser->parseFile("example_1.csv");
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Running the Example
+
+## To run the provided example:
+    Ensure you have PHP installed.
+    Save your CSV file as example_1.csv.
+    Run the script using the command product-parser.php --file example_1.csv --unique-combinations=combination_count.csv.
+
+
+### Future Development
+    parseJSON($filename): To be implemented for parsing JSON files.
+    parseXML($filename): To be implemented for parsing XML files.    
